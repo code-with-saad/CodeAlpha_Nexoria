520e89de757385ca038195e57c766ae0faeb9441
code-with-saad <saadkashif770@gmail.com>
2026-09-25 16:52:09 +0500

    Fix CORS origin for Vercel deployment

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 // Body Parsers & CORS Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || '*',
+  origin: (origin, callback) => callback(null, true),
   credentials: true
 }));
 app.use((req, res, next) => {
